7a07b91cb20cf08f0ae4bfa3ebb1f32612342962
UjuAyoku <63997970+UjuAyoku@users.noreply.github.com>
2020-08-07 00:16:47 -0600

    Add files via upload

--- Number to words converter.py
+++ Number to words converter.py
@@ -1,13 +1,20 @@
 #convert numbers to words
-phone: 1234
+phone = input('Phone: ')
 
-dict = {
-    1: 'One',
-    2: 'Two',
-    3: 'Three',
-    4: 'Four',
-    5: 'Five',
-    6: 
-
-
+digit_mapping = {
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Four',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Nine',
+    '0': 'Zero'
 }
+
+words = ''
+for number in phone:
+    words += digit_mapping.get(number, '!') + ' ' #to space out the words
+print(words)
