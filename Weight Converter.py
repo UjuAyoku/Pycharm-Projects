76a43b8759510e6862a03cd6be7010183735ff1c
UjuAyoku <63997970+UjuAyoku@users.noreply.github.com>
2020-08-08 15:32:33 -0600

    Update Weight Converter.py

--- Weight Converter.py
+++ Weight Converter.py
@@ -1,8 +1,8 @@
-#Converts weight from pounds to kilograms and vice versa
+# Converts weight from pounds to kilograms and vice versa
 
 weight = int(input('Weight: ' ))
 unit = input('(L)bs or (K)g: ')
-#convert from lbs to kg
+# convert from lbs to kg
 if unit.upper() == 'L':
     converter = weight * 0.45359237
     print(f'Your weight is {converter:.2f} kilos')
