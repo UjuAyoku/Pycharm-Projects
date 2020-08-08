ae77da591f799786dc9871d81f756018518cb6b3
UjuAyoku <63997970+UjuAyoku@users.noreply.github.com>
2020-08-08 13:34:57 -0600

    Update Largest Number.py

--- Largest Number.py
+++ Largest Number.py
@@ -1,7 +1,7 @@
-#find the largest number in a list
-list_1 = [31,5,6,10,3,20]
-max_num = list_1[0] #assume the first element of the list is the max number
-for number in list_1:
+# find the largest number in a list
+numbers = [10, 3, 6, 2]
+max_num = numbers[0]  # assume the first element of the list is the max number
+for number in numbers:
     if number > max_num:
         max_num = number
 print(max_num)
