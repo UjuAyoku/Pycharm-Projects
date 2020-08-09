2dddf684ab1b6d84b3a92837e652edb110b74166
UjuAyoku <63997970+UjuAyoku@users.noreply.github.com>
2020-08-08 23:17:15 -0600

    Update Emoji Converter.py

--- Emoji Converter.py
+++ Emoji Converter.py
@@ -1,17 +1,17 @@
-#Convert text to Smileys
+# Convert text to Smileys
 message = input('> ').lower()
 words = message.split(' ')
 
-#mapping special characters to smileys i.e windows + ;
+# mapping special characters to smileys i.e windows + ;
 emojis = {
-    ':)': '😊',  #happy
-    ':(': '😞',  #sad
-    '*': '😘',   #kiss
-    ':x': '🤐',  #lips sealed
-    ':o': '🥱'   #yawn
+    ':)': '😊',  # happy
+    ':(': '😞',  # sad
+    '*': '😘',   # kiss
+    ':x': '🤐',  # lips sealed
+    ':o': '🥱'   # yawn
 }
 
 output = ''
 for word in words:
-    output += emojis.get(word, word) + ' ' #if word is not in emojis, return word
+    output += emojis.get(word, word) + ' '  # if word is not in emojis, return word
 print(output)
